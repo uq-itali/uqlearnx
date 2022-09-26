181379da22865919b3a5379885b2dc7be22d1f29
UQ-Itali <95063901+uq-itali@users.noreply.github.com>
2022-09-27 09:21:42 +1000

    Update copyNextDiv.js

--- js/copyNextDiv.js
+++ js/copyNextDiv.js
@@ -4,7 +4,7 @@ window.addEventListener('load',function(){
             var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
                 return new bootstrap.Tooltip(tooltipTriggerEl)
             });
-}};
+});
             
 function copyButton(){
             /*Copy code function. Button in the HTML has an onclick trigger that runs this. Only way I got it to work in LearnX.*/
