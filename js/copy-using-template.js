feb478197cc7dd0680d8ea0ccacab6dec6a6d535
Tim Scholl <t.scholl@uq.edu.au>
2022-10-19 16:23:13 +1000

    change copycode func to onclick element on page

--- js/copy-using-template.js
+++ js/copy-using-template.js
@@ -7,9 +7,8 @@ window.addEventListener('load',function(){
             var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
                 return new bootstrap.Tooltip(tooltipTriggerEl)
             });
-            $('.htmlToCopy').after('<button type="button" class="btn btn-primary copybutton mt-3"><span style="color: var(--bs-bg-color) !important" class="fa fa-clipboard" aria-hidden="true"></span>&nbsp;Copy code</button>');
-            $('.copybutton').on('click', function(event) {
                     /*Copy code function. Button in the HTML has an onclick trigger that runs this. Only way I got it to work in LearnX.*/
+                    function copyCode(){
                     var btn = $(this);
                     var btnHtml = $(this).html();
                     var snippet = btn.prev().html().slice(12,-12); //this removes the outer div and whitespace the html is wrapped in to leave just the relevant html.
@@ -19,7 +18,7 @@ window.addEventListener('load',function(){
                     window.setTimeout(function() {
                         btn.html(btnHtml);
                      }, 3000);
-            });
+            };
                /*Newer version of copy to clipboard*/
                    function copyTextToClipboard(text) {
                        if (!navigator.clipboard) {
