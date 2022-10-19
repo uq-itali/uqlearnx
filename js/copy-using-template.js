f5bd38f655aa1861601901882fe621b71c338d98
Tim Scholl <t.scholl@uq.edu.au>
2022-10-19 14:36:00 +1000

    adding custom CSS to build guide and updating JS

--- js/copy-using-template.js
+++ js/copy-using-template.js
@@ -44,4 +44,18 @@ window.addEventListener('load',function(){
                        }
                        document.body.removeChild(textArea);
                    }
+                   /*JS required for the collapsible items*/
+                   var coll = document.getElementsByClassName("collapsible");
+                   var i;
+                   for (i = 0; i < coll.length; i++) {
+                    coll[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        var content = this.nextElementSibling;
+                        if (content.style.maxHeight){
+                            content.style.maxHeight = null;
+                        } else {
+                            content.style.maxHeight = content.scrollHeight + "px";
+                        }
+                    });
+                }
             });
