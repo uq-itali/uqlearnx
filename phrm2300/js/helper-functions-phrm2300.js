0b42c05181282907afd6e74709dfc95293c6e7cc
Tim Scholl <t.scholl@uq.edu.au>
2023-01-14 08:58:24 +1000

    change click trigger for bookmarks button

--- phrm2300/js/helper-functions-phrm2300.js
+++ phrm2300/js/helper-functions-phrm2300.js
@@ -29,7 +29,7 @@ window.addEventListener('load',function(){
                 } */        
     //Ensure all functions also happen again when a user navigates to the next or previous unit page.
     //Always duplicate any function that needs to happen on loading of a page to be inside the function that follows.
-               $('button.seq_other').on('click', function(event){
+               $('button.tab').on('click', function(event){
                 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
                 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
                     return new bootstrap.Tooltip(tooltipTriggerEl)
