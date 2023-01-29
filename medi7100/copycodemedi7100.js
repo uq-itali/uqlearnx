2f2fa6e41c4c455a459a56f0e7208ed922fd2eca
uq-itali <t.scholl@uq.edu.au>
2023-01-29 15:11:24 +1000

    Changes to bookmark code

--- medi7100/copycodemedi7100.js
+++ medi7100/copycodemedi7100.js
@@ -23,11 +23,17 @@ window.addEventListener('load',function(){
         }, 2000);
     });
 /*Make the go to bookmarks link next to the bookmark this page link*/
+var bookmarkCount = $('.gotobookmarksbtn').length
+if(bookmarkCount < 1){
     var currentLocation = window.location.href;
         var bookmarkSlice = currentLocation.slice(32,65);
         var bookmarkURL = 'window.open(\'/courses'+bookmarkSlice+'/bookmarks/\',\'_blank\')';
         $('.bookmark-button-wrapper').append('<button class="btn btn-link gotobookmarksbtn" onclick=""><span class="bookmark-text">Go to bookmarks <i class="fa-solid fa-external-link-square"></i></button>')
         $('.gotobookmarksbtn').attr('onclick',bookmarkURL);
+}
+else{
+    console.log('Go to bookmarks already present')
+}
 /*Make the bug/suggestion form link next to the go to bookmarks link*/
 if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhhugo' || $('span.username').text() == 'uqkcosg2' || $('span.username').text() == 'uqkward1' || $('span.username').text() == 'uqcyoun3' || $('span.username').text() == 'uqscoore'){
     var formLinkCount = $('.bugOrSuggestion').length
@@ -40,7 +46,7 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
             console.log('Form link already present')
         } 
 }
-    
+/*Toggle icons in any accordions*/
         $('a[data-bs-toggle="collapse"]').on('click', function(){
             if($('a[data-bs-toggle="collapse"] i').hasClass('fa-angle-down') || $('a[data-bs-toggle="collapse"] i').hasClass('fa-angle-up') == true){
                 $(this).children().toggleClass('fa-angle-down fa-angle-up');
@@ -55,7 +61,8 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
                             $(this).children().toggleClass('fa-caret-down fa-caret-up');
                            }
                        });
-                       $('div.sequence-nav, nav.sequence-bottom').on('click', function(event){
+/*When a user changes tabs or navigates to the next page repeat functions*/
+$('div.sequence-nav, nav.sequence-bottom').on('click', function(event){
 /*Initialise all tooltips when person goes to next page*/
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
@@ -64,7 +71,7 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
 
 /*Copy email code. Make a bootstrap badge after an email address and give it the copyBadge class for this to function*/
         var $temp = $('<input>');
-        $('.copyBadge').on('click', function () {
+        $('.copyBadge').on('click', function (){
             var $copyBadge = $(this);
             var $url = $(this).prev().attr('href');
             var $copyBadgeHTML = $(this).html();
@@ -96,7 +103,7 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
         $('.bookmark-button-wrapper').append('<button class="btn btn-link gotobookmarksbtn" onclick=""><span class="bookmark-text">Go to bookmarks <i class="fa-solid fa-external-link-square"></i></button>')
         $('.gotobookmarksbtn').attr('onclick',bookmarkURL);
         var bookmarkCount = $('.gotobookmarksbtn').length
-        if(bookmarkCount <= 1){
+        if(bookmarkCount < 1){
             var currentLocation = window.location.href;
             var bookmarkSlice = currentLocation.slice(32,65);
             var bookmarkURL = 'window.open(\'/courses'+bookmarkSlice+'/bookmarks/\',\'_blank\')';
@@ -106,6 +113,7 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
             else{
                 console.log('Go to bookmarks already present');
             }
+/*Toggle icons in any accordions*/
             $('a[data-bs-toggle="collapse"]').on('click', function(){
                 if($('a[data-bs-toggle="collapse"] i').hasClass('fa-angle-down') || $('a[data-bs-toggle="collapse"] i').hasClass('fa-angle-up') == true){
                     $(this).children().toggleClass('fa-angle-down fa-angle-up');
@@ -163,13 +171,4 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
                 $('.blownUpImage').attr('src', $(this).find('img').attr('src'));
                 $('#imagemodal').modal('show');   
             });
-            var formLinkCount = $('.bugOrSuggestion').length
-                if(formLinkCount <= 0){
-                    var formsLink = 'window.open(\'https://forms.office.com/r/5kNvhxasnB\')';
-                $('.bookmark-button-wrapper').append('<button class="btn btn-link bugOrSuggestion" onclick=""><span class="bookmark-text">Go to bug/suggestion form <span class="fa-solid fa-bug"></span></span></button>')
-                $('.bugOrSuggestion').attr('onclick',formsLink);
-                }
-                else{
-                    console.log('Form link already present')
-                } 
         });
