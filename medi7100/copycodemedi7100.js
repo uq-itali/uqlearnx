4e2ed0a19bb603f66badc188c9e792740a49d521
uq-itali <t.scholl@uq.edu.au>
2023-04-12 08:52:47 +1000

    fix alert + tinyMCE content pickup on accordion

--- medi7100/copycodemedi7100.js
+++ medi7100/copycodemedi7100.js
@@ -22,7 +22,85 @@ function scrollFunction(){
     mybutton.style.display = "none";
   }
 }
+   /*On initial load, create an empty div for the alerts to sit in.*/
+   $('.sequence-nav').after('<div id="pageStyleAlert"></div>');
+   /*HTML for an alert when a page is read-only. That is, it contains no activities or videos.*/
+   var readOnlyAlert = '<div class="alert alert-info border border-3 border-info  rounded read-only-alert">\n<h4 class="alert-heading text-center"><i class="fa-solid fa-book"></i> Read Only Page</h4>\n<p class="text-center"><strong>Stay on this page for 6 seconds to receive the completion tick</strong>.</p>\n<p>This page is considered a read only page (if there is video or activity content it is external to the LearnX platform). To have the page ticked in the navbar at the top of screen, you need to stay present on the page for at least 6 seconds, and then click to the next page and the completion tick will appear.</p>\n<ul class="mb-0">\<li><strong>If this page contains video</strong>, it is embedded via other means than the LearnX video component type, and you can watch as much or as little as you would like, it will not affect the completion tick.</li>\n<li><strong>If the page contains activities</strong>, they are not native LearnX activities or LTI embedded LearnX activities, completing them is your choice, it will not affect the completion tick.</li>\n</ul>\n<p class="mb-0 text-center"><strong>To close these alerts click the eye icon - <i class="fa-solid fa-eye"></i> in the bottom right of screen. You can reveal them again by clicking the eye-slash - <i class="fa-solid fa-eye-slash"></i> - icon.</strong></p>\n</div>\n';
 
+   /*HTML for an alert when a page contains activities only.*/
+   var activitiesAlert = '<div class="alert alert-primary border border-3 border-primary rounded activities-alert">\n<h4 class="alert-heading text-center"><i class="fa-solid fa-pen-to-square"></i> Activities/Problems Page</h4>\n<p class="text-center"><strong>Complete and submit to all activities on this page to receive the completion tick</strong>.</p>\n<p>This page contains LearnX problems (MCQ, fill in the blank etc.) or H5P material linked via LTI. Complete all the activities and then click to the next page and the completion tick will appear.</p>\n<p class="mb-0"><strong>If this page contains video</strong>, it is embedded via other means than the LearnX video component type, and you can watch as much or as little as you would like, it will not affect the completion tick.</p>\n<p class="mb-0 text-center"><strong>To close these alerts click the eye icon - <i class="fa-solid fa-eye"></i> in the bottom right of screen. You can reveal them again by clicking the eye-slash - <i class="fa-solid fa-eye-slash"></i> - icon.</strong></p>\n</div>\n';
+
+   /*HTML for an alert when a page contains videos only.*/
+   var videoAlert = '<div class="alert alert-dark border border-3 border-dark rounded video-alert">\n<h4 class="alert-heading text-center"><i class="fa-solid fa-film"></i> Video Page</h4>\n<p class="text-center"><strong>For each video on this page you must watch 95% or more of each video on this page to receive the completion tick</strong>.</p>\n<p>This page has a LearnX video component type present. You need to watch 95% of the video, and then click to the next page and the completion tick will appear. Remember you can use the speed control to watch the video at a faster pace than normal. To calculate the time required - (mins x 60 + any secs) x 0.95. E.g. 5mins 32secs = (5 x 60 + 32) x 0.95 = 332 x 0.95 = 315.4 secs / 60 = 5mins 16secs to get to the required 95%. Once this is completed, click to go to the next page and the completion tick will appear.</p>\n<p class="mb-0"><strong>If the page contains activities</strong>, they are not native LearnX activities or LTI embedded LearnX activities, completing them is your choice, it will not affect the completion tick.</p>\n<p class="mb-0 text-center">To close these alerts click the eye icon - <i class="fa-solid fa-eye"></i> in the bottom right of screen. You can reveal them again by clicking the eye-slash - <i class="fa-solid fa-eye-slash"></i> - icon.</strong></p>\n</div>';
+
+   /*HTML for an alert when a page contains both videos and activities.*/
+   var videoPlusActivitiesAlert = '<div class="alert alert-uq border border-3 border-uq rounded video-plus-activities-alert">\n<h4 class="alert-heading text-center"><i class="fa-solid fa-pen-to-square"></i> + <i class="fa-solid fa-film"></i> Problem + Videos Page</h4>\n<p class="text-center"><strong>Complete and submit all activities, </strong>and<strong> watch 95% or more of each video on this page to receive the completion tick</strong>.</p>\n<p class="mb-0">This page contains both videos and activities that are connected to the LearnX platform. Once you have completed all activities and watched 95% or more of each video you can click to go to the next page and the completion tick will appear.</p>\n\n<p class="mb-0 text-center">To close these alerts click the eye icon - <i class="fa-solid fa-eye"></i> in the bottom right of screen. You can reveal them again by clicking the eye-slash - <i class="fa-solid fa-eye-slash"></i> - icon.</strong></p>\n</div>';
+
+    $('body').prepend('<button  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Click this button to show/hide the completion alerts." id="reopenCompletionAlerts" class="btn btn-success"  title="Toggle hide/show completion alerts"><span class="fa-solid"></span></button>');
+   if(localStorage.getItem("completionAlert") == "visible" || localStorage.getItem("completionAlert") == null){
+      $('#reopenCompletionAlerts span').addClass('fa-eye');
+       $('#pageStyleAlert').removeClass('d-none');
+        console.log('eye should be open');
+    }
+    else if(localStorage.getItem("completionAlert") == "hidden"){
+        $('#reopenCompletionAlerts span').addClass('fa-eye-slash');
+        $('#pageStyleAlert').addClass('d-none');
+        console.log('eye should be closed');
+    }
+     $('#reopenCompletionAlerts').on('click',function(){
+        if($('#reopenCompletionAlerts span').hasClass('fa-eye') == true){
+            localStorage.setItem("completionAlert", "hidden");
+            $('#pageStyleAlert').addClass('d-none');
+            $('#reopenCompletionAlerts span').toggleClass('fa-eye fa-eye-slash');
+            console.log('alerts closed for session');
+        }
+        else if($('#reopenCompletionAlerts span').hasClass('fa-eye-slash') == true){
+            localStorage.setItem("completionAlert", "visible");
+            $('#pageStyleAlert').removeClass('d-none');
+            $('#reopenCompletionAlerts span').toggleClass('fa-eye-slash fa-eye');
+            console.log('alerts open for session');
+        }
+    })
+
+   
+   /*Tell me the number of problems, videos and LTI Consumer links that are on the page.*/
+   var problemClass = $('.problem').length;
+   var videoClass = $('.video').length;
+   var ltiClass = $('.lti_consumer').length;
+
+   /*Cater for all combinations of content and display the relative completion alert*/
+   if(videoClass != 0 && problemClass != 0 && ltiClass != 0){
+    //alert('videos, problems and LTI present');
+    $('div#pageStyleAlert').html(videoPlusActivitiesAlert);
+}
+else if(videoClass != 0 && problemClass != 0){
+    //alert('video and problems present')
+    $('div#pageStyleAlert').html(videoPlusActivitiesAlert)
+}
+else if(videoClass != 0 && ltiClass != 0){
+    //alert('videos and LTI present')
+    $('div#pageStyleAlert').html(videoPlusActivitiesAlert)
+}
+else if(ltiClass != 0 && problemClass != 0){
+   // alert('lti and problems present')
+    $('div#pageStyleAlert').html(activitiesAlert)
+}
+else if(videoClass != 0){
+  //  alert('video present')
+    $('div#pageStyleAlert').html(videoAlert)
+}
+else if(problemClass != 0){
+  //  alert('problem present')
+    $('div#pageStyleAlert').html(activitiesAlert)
+}
+else if(ltiClass != 0){
+  //  alert('LTI present')
+    $('div#pageStyleAlert').html(activitiesAlert)
+}
+else{
+   // alert('read only page')
+    $('div#pageStyleAlert').html(readOnlyAlert)
+}
 /*Copy email code. Make a bootstrap badge after an email address and give it the copyBadge class for this to function*/
 var $temp = $('<input>');
     $('.copyBadge').on('click', function () {
@@ -83,9 +161,7 @@ $('div.sequence-nav, nav.sequence-bottom').on('click', function(event){
             return new bootstrap.Tooltip(tooltipTriggerEl)
         });
 
-/*Back to top button*/
-    $('body').prepend('<button class="btn btn-dark" onclick="javascript:document.body.scrollTop = 0;document.documentElement.scrollTop = 0;" id="bttBtn" title="Go to top"><span class="fa-solid fa-arrow-up"></span></button>');
-    // Get the button
+   // Get the button
     let mybutton = document.getElementById("bttBtn");
     
     // When the user scrolls down 20px from the top of the document, show the button
@@ -98,6 +174,57 @@ $('div.sequence-nav, nav.sequence-bottom').on('click', function(event){
         mybutton.style.display = "none";
       }
     }
+    /*Check to see if the empty div is at the top of screen. If it is there, do nothing, if not, create it.*/
+    if($('.pageStyleAlert').length != 0){
+        $('.sequence-nav').after('<div id="pageStyleAlert"></div>');}
+        else{
+            //do nothing
+        }
+
+   if(localStorage.getItem("completionAlert") == "visible"){
+      $('#reopenCompletionAlerts span').addClass('fa-eye');
+       $('#pageStyleAlert').removeClass('d-none');
+        console.log('eye should be open');
+    }
+    else if(localStorage.getItem("completionAlert") == "hidden"){
+        $('#reopenCompletionAlerts span').addClass('fa-eye-slash');
+        $('#pageStyleAlert').addClass('d-none');
+        console.log('eye should be closed');
+    }
+        /*Catering for all combinations of content types and injecting the correct alert into the div at the top of screen.*/
+        if(videoClass != 0 && problemClass != 0 && ltiClass != 0){
+         //   alert('videos, problems and LTI present')
+            $('div#pageStyleAlert').html(videoPlusActivitiesAlert)
+        }
+        else if(videoClass != 0 && problemClass != 0){
+         //   alert('video and problems present')
+            $('div#pageStyleAlert').html(videoPlusActivitiesAlert)
+        }
+        else if(videoClass != 0 && ltiClass != 0){
+         //   alert('videos and LTI present')
+            $('div#pageStyleAlert').html(videoPlusActivitiesAlert)
+        }
+        else if(ltiClass != 0 && problemClass != 0){
+        //    alert('lti and problems present')
+            $('div#pageStyleAlert').html(activitiesAlert)
+        }
+        else if(videoClass != 0){
+         //   alert('video present')
+            $('div#pageStyleAlert').html(videoAlert)
+        }
+        else if(problemClass != 0){
+        //    alert('problem present')
+            $('div#pageStyleAlert').html(activitiesAlert)
+        }
+        else if(ltiClass != 0){
+         //   alert('LTI present')
+            $('div#pageStyleAlert').html(activitiesAlert)
+        }
+        else{
+         //   alert('read only page')
+            $('div#pageStyleAlert').html(readOnlyAlert)
+        }
+        });
     
 
 /*Copy email code. Make a bootstrap badge after an email address and give it the copyBadge class for this to function*/
@@ -196,4 +323,3 @@ if($('span.username').text() == 'uqtscho3' || $('span.username').text() == 'uqhh
                 $('.blownUpImage').attr('src', $(this).find('img').attr('src'));
                 $('#imagemodal').modal('show');   
             });
-        });
